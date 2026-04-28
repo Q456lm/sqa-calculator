5e4d6b8f0f710188a9ea8d66df4f6623f547a729
Clarence Bunting <cbbunting1@my.waketech.edu>
2026-04-28 07:43:52 -0400

    Added all 8 methods

--- calculator.py
+++ calculator.py
@@ -1,1 +1,35 @@
+import math
 
+class Calculator:
+    
+    def add(a,b):
+        return a + b
+    
+    def subtract(a,b):
+        return a - b
+    
+    def multiply(a,b):
+        return a * b
+    
+    def divide(a,b):
+        if (b == 0):
+            raise ZeroDivisionError
+        return a / b
+    
+    def power(base,exp):
+        return pow(base,exp)
+    
+    def square_root(a):
+        if (a < 0):
+            raise ValueError
+        return math.sqrt(a)
+
+    def modulus(a,b):
+        if (b == 0):
+            raise ZeroDivisionError
+        return a%b
+
+    def floor_divide(a,b):
+        if (b == 0):
+            raise ZeroDivisionError
+        return a//b
